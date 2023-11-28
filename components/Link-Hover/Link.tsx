fa1c9d48940124e416f53f9da4735d02af1fdadb
Zain Ali <ZainSoftwear11@gmail.com>
2023-11-28 09:38:40 +0500

    Link component completed

---
+++ components/Link-Hover/Link.tsx
@@ -0,0 +1,13 @@
+import PerspectiveText from "./PerspectiveText";
+
+export default function Link() {
+	return (
+		<div className="button">
+			<div className="slider">
+				<div className="el">
+					<PerspectiveText label="Home" />
+				</div>
+			</div>
+		</div>
+	);
+}
